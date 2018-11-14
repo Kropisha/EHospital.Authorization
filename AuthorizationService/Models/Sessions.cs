7b4bd5ada78bd5856e28454ba1d8f0dd42897dd4
Julia Kropivnaya <kropisha@gmail.com>
2018-11-14 08:38:11 +0200

    Fetch some logic

--- AuthorizationService/Models/Sessions.cs
+++ AuthorizationService/Models/Sessions.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eHospital.Authorization
 {
     public class Sessions
     {
         [Key]
-        //[DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int SessionId { get; set; }
 
-      //  [ForeignKey("UsersData")]
         public int UserId { get; set; }
 
         public string Token { get; set; }
